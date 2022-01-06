aed23cce9a132b51ffa0c4ac269bdb27128ce8b0
unknown <ian.napoli8@gmail.com>
2022-01-06 12:09:04 -0800

    added practice files

---
+++ practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numOfJudges;
+    //contestant array
+    const int contestantNumber[10][10];
+
+    cout<<"Please enter the number of judges:\n";
+    cin>>numOfJudges;
+
+    cout<<"Please enter the contestant number, followed by the judge's scores.\n";
+    cin>>contestantNumber;
+
+
+    return 0;
+}
