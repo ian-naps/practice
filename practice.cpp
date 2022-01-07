f6570371b1f9f0393280c98114a0852b8dc8c661
unknown <ian.napoli8@gmail.com>
2022-01-06 17:32:14 -0800

    added contestant loop

--- practice.cpp
+++ practice.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
+
 int main(){
     int numOfJudges;
     //contestant array
-    const int contestantNumber[10][10];
+    const int scoreArray = 10;
+    int contestantLoopCounter=0;
+    int contestantNumber[scoreArray];
 
-    cout<<"Please enter the number of judges:\n";
-    cin>>numOfJudges;
+    cout << "Please enter the number of judges:\n";
+    cin >> numOfJudges;
 
-    cout<<"Please enter the contestant number, followed by the judge's scores.\n";
-    cin>>contestantNumber;
+    cout << "Please enter the contestant number, followed by the judge's scores.\n";
+    while (contestantNumber > 0) {
+
+    cin >> contestantNumber[contestantLoopCounter];
+    contestantLoopCounter++;
+    }
+
 
 
     return 0;
